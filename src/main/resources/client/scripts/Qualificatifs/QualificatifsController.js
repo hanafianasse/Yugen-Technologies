7193addab09145b8fd4b6836af4b5abced84cefc
HASSOUNI AMAL <amalhassouni94@gmail.com>
2017-03-10 11:23:04 +0100

    Qualificatif controller FE

--- src/main/resources/client/scripts/Qualificatifs/QualificatifsController.js
+++ src/main/resources/client/scripts/Qualificatifs/QualificatifsController.js
@@ -1,3 +1,17 @@
 /**
  * Created by DOSI on 10/03/2017.
  */
+'use strict';
+
+
+angular.module('app').controller('QualificatifCtrl', ['$scope','$routeParams','$http','QualificatifService',function ($scope,$routeParams,$http, QualificatifService) {
+
+    $scope.qualificatifs = [];
+
+    QualificatifService.getAll(function (data) {
+        $scope.qualificatifs = data;
+
+    });
+
+
+}]);
