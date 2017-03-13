44d8f3562ba75ff6b5f4156b39921fe567bdd176
HASSOUNI AMAL <amalhassouni94@gmail.com>
2017-03-13 14:07:09 +0100

    Front end qualificatif

--- src/main/resources/client/scripts/Qualificatifs/QualificatifsService.js
+++ src/main/resources/client/scripts/Qualificatifs/QualificatifsService.js
@@ -1,11 +1,9 @@
 
 
 angular.module('app')
-<<<<<<< HEAD
+
     .factory('QualificatifService', ['$http','$location', function ($http,$location) {
-=======
-    .factory('QualificatifService', ['$http', function ($http) {
->>>>>>> 9e4c5370a7650aaca884753de9e7c8df71530419
+
 
         var urlBase = 'http://localhost:8090/qualificatif';
         var dataFactory = {};
@@ -20,10 +18,7 @@ angular.module('app')
 
         dataFactory.addQualificatif = function (entity) {
             return $http.post(urlBase, entity);
-<<<<<<< HEAD
 
-=======
->>>>>>> 9e4c5370a7650aaca884753de9e7c8df71530419
         };
 
         dataFactory.updateQualificatif = function (entity) {
@@ -31,11 +26,10 @@ angular.module('app')
         };
 
         dataFactory.deleteQualificatif = function (id) {
-<<<<<<< HEAD
+
             return $http.delete(urlBase + '/' + id);
-=======
-            return $http.delete(urlBase + '/remove/' + id);
->>>>>>> 9e4c5370a7650aaca884753de9e7c8df71530419
+
+
         };
 
         return dataFactory;
