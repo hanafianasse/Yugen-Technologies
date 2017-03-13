f5b99ef7fb9081cde9b76bf9933ebe32c38e2914
red1 <rzakraoui@gmail.com>
2017-03-13 17:46:32 +0100

    WebServiceRubrique

---
+++ src/main/java/fr/univbrest/dosi/spi/controller/RubriqueController.java
@@ -0,0 +1,58 @@
+package fr.univbrest.dosi.spi.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import fr.univbrest.dosi.spi.bean.Rubrique;
+
+import fr.univbrest.dosi.spi.service.RubriqueService;
+
+/**
+ * @author DOSI
+ *
+ */
+@RestController
+@RequestMapping(value = "/rubrique")
+public class RubriqueController {
+	
+	@Autowired
+	private RubriqueService rubriqueService;
+
+	@RequestMapping( method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public final Rubrique addRubrique(@RequestBody final Rubrique rubrique) {
+		return rubriqueService.addRubrique(rubrique);
+	}
+	/**
+	 * 
+	 * @param rubrique
+	 * @return
+	 */
+	
+	@RequestMapping(method = RequestMethod.PUT, headers = "Accept=application/json")
+	public final Rubrique editRubrique(@RequestBody final Rubrique rubrique) {
+		return rubriqueService.updateRubrique(rubrique);
+	}
+
+	
+
+	
+	@RequestMapping(produces = "application/json")
+	public final Iterable<Rubrique> GetRubrique() {
+		final Iterable<Rubrique> rubriques = rubriqueService.listRubriques();
+		return rubriques;
+	}
+
+	
+	@RequestMapping(value = "/delete/{id_rubrique}",method = RequestMethod.DELETE, headers = "Accept=application/json")
+	public final void removeRubrique(@PathVariable("id_rubrique") final Long id_rubrique) {
+		rubriqueService.deleteRubrique(id_rubrique);
+	}
+}
+
---
+++ src/main/java/fr/univbrest/dosi/spi/dao/RubriqueRepository.java
@@ -0,0 +1,12 @@
+package fr.univbrest.dosi.spi.dao;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import fr.univbrest.dosi.spi.bean.Rubrique;
+
+@RepositoryRestResource(collectionResourceRel = "rubrique", path = "rubrique")
+public interface RubriqueRepository extends PagingAndSortingRepository <Rubrique, Long> { 
+	
+
+}
