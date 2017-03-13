8d8dc43a00bdf544e294f24db73777b6196d9f41
Yann Pauly <yann.pauly@gmail.com>
2017-03-13 15:47:13 +0100

    Tst unitaire webservice Question

--- src/test/java/fr/univbrest/dosi/spi/controller/QuestionControllerTest.java
+++ src/test/java/fr/univbrest/dosi/spi/controller/QuestionControllerTest.java
@@ -17,136 +17,145 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 
 import fr.univbrest.dosi.spi.bean.Etudiant;
 import fr.univbrest.dosi.spi.bean.Question;
+import fr.univbrest.dosi.spi.bean.Question;
+import fr.univbrest.dosi.spi.service.QuestionService;
 
+@RunWith(MockitoJUnitRunner.class)
 public class QuestionControllerTest {
-	Question q;
+	
+	@InjectMocks
+	QuestionController QuestionController;
 
-	@Before
-	public void init() {
+	@Mock
+	QuestionService QuestionService;
+
+	String url = "http://localhost:8090/question";
+
+	@Test
+	public void doitAjouterQuestion() throws ClientProtocolException,
+			IOException
+	{
+		/**
+		 * Instanciation d'un nouveau Question
+		 */
+		Question q = new Question();
 		q = new Question();
 		q.setIdQuestion(new Long(26));
-		q.setIdQualificatif(new BigDecimal(1));
 		q.setIntitule("q1");
 		q.setNoEnseignant(new BigDecimal(2));
 		q.setType("QUS");
-
+		
+		/**
+		 * Test de l'ajout du Question
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").body(q).when()
+				.post(url).then().statusCode(200);
 	}
 
 	@Test
-	public void addQuestionTest() throws ClientProtocolException, IOException {
+	public void doitModifierQuestion()
+	{
+		/**
+		 * Instanciation d'un nouveau Question
+		 */
+		Question q = new Question();
+		q.setIdQuestion(new Long(26));
+		q.setIntitule("q1");
+		q.setNoEnseignant(new BigDecimal(2));
+		q.setType("QUS");
+		
 
-		// Creation de qst et dune requete POST
-		final HttpClient qst = HttpClientBuilder.create().build();
-		final HttpPost mockRequestPost = new HttpPost(
-				"http://localhost:8090/question");
+		/**
+		 * Test de l'ajout du Question
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").body(q).when()
+				.post(url).then().statusCode(200);
 
-		// creation de l'objet mapper afin de convertir l'objet en jsonInSTring
-		final ObjectMapper mapper = new ObjectMapper();
-		com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer()
-				.withDefaultPrettyPrinter();
-		String jsonInString = ow.writeValueAsString(q);
+		/**
+		 * Mise à jour de la réponse
+		 */
+		q.setIntitule("questionUp");
 
-		// (header+body)
-		mockRequestPost.addHeader("Content-Type", "application/json");
-		mockRequestPost.setEntity(new StringEntity(jsonInString));
-
-		// creation de la reponse
-		final HttpResponse mockResponse = qst.execute(mockRequestPost);
-		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
-
+		/**
+		 * Test de la mise à jour
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").body(q).when()
+				.put(url).then().statusCode(200);
 	}
 
 	@Test
-	public void listerQuestionTest() throws ClientProtocolException,
-			IOException {
+	public void doitSupprimerQuestion()
+	{
+		/**
+		 * Instanciation d'un nouveau Question
+		 */
+		Question q = new Question();
+		q.setIdQuestion(new Long(26));
+		q.setIntitule("q1");
+		q.setNoEnseignant(new BigDecimal(2));
+		q.setType("QUS");
+		
+		/**
+		 * Test de l'ajout du Question
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").body(q).when()
+				.post(url).then().statusCode(200);
 
-		final HttpClient client = HttpClientBuilder.create().build();
-		final HttpGet mockRequest = new HttpGet(
-				"http://localhost:8090/question");
-		final HttpResponse mockResponse = client.execute(mockRequest);
-
-		// Le code retour HTTP doit être un succès (200)
-		// Assert.assertEquals(200,mockResponse.getStatusLine().getStatusCode());
-
-		final BufferedReader rd = new BufferedReader(new InputStreamReader(
-				mockResponse.getEntity().getContent()));
-		final ObjectMapper mapper = new ObjectMapper();
-		final Iterable<Question> qst = mapper.readValue(rd, Iterable.class);
-
-		Assert.assertNotNull(qst);
-
+		/**
+		 * Test de la suppression du Question
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").when().delete(url + "/delete/26")
+				.then().statusCode(200);
 	}
 
 	@Test
-	public void getQuestionTest() throws ClientProtocolException, IOException {
-		final HttpClient client = HttpClientBuilder.create().build();
-		final HttpGet mockRequest = new HttpGet(
-				"http://localhost:8090/question/1");
-		final HttpResponse mockResponse = client.execute(mockRequest);
-		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
+	public void doitRecupererQuestion()
+	{
+		/**
+		 * Instanciation d'un nouveau Question
+		 */
+		Question q = new Question();
+		q.setIdQuestion(new Long(26));
+		q.setIntitule("q1");
+		q.setNoEnseignant(new BigDecimal(2));
+		q.setType("QUS");
+		
+		/**
+		 * Test de l'ajout du Question
+		 */
+		RestAssuredMockMvc.given().standaloneSetup(QuestionController)
+				.contentType("application/json").body(q).when()
+				.post(url).then().statusCode(200);
+		
+		/**
+		 * Vérification du status de la requête
+		 */
+		RestAssured.given().when().get(url + "/26").then().statusCode(200);
 	}
-
-	/*@Test
-	public final void deleteQuestionTest() throws ClientProtocolException,
-			IOException {
-
-		// Creation de qst et dune requete POST
-		final HttpClient qst = HttpClientBuilder.create().build();
-		final HttpPost mockRequestPost = new HttpPost(
-				"http://localhost:8090/question");
-
-		// creation de l'objet mapper afin de convertir l'objet en jsonInSTring
-		final ObjectMapper mapper = new ObjectMapper();
-		com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer()
-				.withDefaultPrettyPrinter();
-		String jsonInString = ow.writeValueAsString(q);
-
-		// (header+body)
-		mockRequestPost.addHeader("Content-Type", "application/json");
-		mockRequestPost.setEntity(new StringEntity(jsonInString));
-
-		// creation de la reponse
-		final HttpResponse mockResponse = qst.execute(mockRequestPost);
-		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
-
-		// Création du client et éxécution d'une requete GET
-		final HttpClient client = HttpClientBuilder.create().build();
-		final HttpDelete mockRequest = new HttpDelete(
-				"http://localhost:8090/question/delete/26");
-		final HttpResponse mockResponseDelete = client.execute(mockRequest);
-
-		// creation de l'objet mapper afin de convertir l'objet en jsonInSTring
-		final ObjectMapper mapper = new ObjectMapper();
-		com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer()
-				.withDefaultPrettyPrinter();
-		String jsonInString = ow.writeValueAsString(q);
-
-		// (header+body)
-		mockRequest.addHeader("Content-Type", "application/json");
-		mockRequest.setEntity(new StringEntity(jsonInString));
-
-		// Le code retour HTTP doit être un succès (200)
-		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
-
-	}*/
 
 	@Test
-	public final void UpdateQuestionTest() throws ClientProtocolException,
-			IOException {
-
-		// Création du client et éxécution d'une requete Put
-		final HttpClient client = HttpClientBuilder.create().build();
-		final HttpPut mockRequest = new HttpPut(
-				"http://localhost:8090/question");
-		final HttpResponse mockResponse = client.execute(mockRequest);
-
-		// Le code retour HTTP doit être un succès (200)
-		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
-
+	public void doitRecupererQuestions()
+	{
+		/**
+		 * Vérification du status de la requête
+		 */
+		RestAssured.given().when().get(url).then().statusCode(200);
 	}
+
 }
