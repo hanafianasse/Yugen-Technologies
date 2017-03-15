8bfd85b28c0a95006a886dcb9f09b74269cafdf5
HASSOUNI AMAL <amalhassouni94@gmail.com>
2017-03-15 17:14:42 +0100

    creation service dashboard

---
+++ src/main/resources/client/scripts/Dashboard/DashboardController.js
@@ -0,0 +1,4 @@
+/**
+ * Created by DOSI on 15/03/2017.
+ */
+angular.module('app').controller('DashboardController', ['$scope', '$location','$routeParams', 'EtudiantsService', 'formationService','PromotionsService','enseignantsFactory',function ($scope,$route,$routeParams,EtudiantsService, formationService,PromotionsService,enseignantsFactory){}
