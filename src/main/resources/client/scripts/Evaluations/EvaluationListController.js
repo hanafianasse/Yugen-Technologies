155ed0eb28fbc97e2dc95a04f83fab0e7b7d43f7
Marwane el atrassi <Atrassi68@gmail.com>
2017-03-23 09:16:37 +0100

    IHM eva

--- src/main/resources/client/scripts/Evaluations/EvaluationListController.js
+++ src/main/resources/client/scripts/Evaluations/EvaluationListController.js
@@ -3,11 +3,27 @@
  */
 'use strict';
 
-angular.module('app').controller('EvaluationListCtrl', ['$scope','$route','$rootScope','$routeParams','$http','$location','EvaluationService','$modal',function ($scope,$route,$rootScope,$routeParams,$http,$location, EvaluationService,$modal) {
+angular.module('app').controller('EvaluationListCtrl', ['$scope','$route','$rootScope','$routeParams','$http','$location','EvaluationService','RubriqueService','$modal',function ($scope,$route,$rootScope,$routeParams,$http,$location, EvaluationService, RubriqueService,$modal) {
 
 
+console.log("je suis dans EvaluationListController");
 
+//Recupération de toutes les évaluations
+var promiseEvaluation = EvaluationService.getAll();
+promiseEvaluation.success(function(data) {
+	$scope.evaluations = data;
+	console.log($scope.evaluations);
+}).error(function(data) {
+	console.log("get evaluation: erreur");
+});
 
+//recupération des rubriques 
+
+$scope.select = function(evaluation){
+	console.log('affichage des rubriques liées a l\'évaluation'+ evaluation.designation);
+	
+	
+}
 
 
 }]);
